fbf0750a7e1501bd9a927120056ca140b563ee3b
IociaNetwork <iocianetwork@gmail.com>
2017-08-16 22:02:47 -0600

    Set up workspace

---
+++ src/main/java/iocia/network/minecraft/plugins/iutilities/IUtilitiesMain.java
@@ -0,0 +1,20 @@
+package iocia.network.minecraft.plugins.iutilities;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class IUtilitiesMain extends JavaPlugin {
+    /*---Data---*/
+
+    /*---Constructors---*/
+
+    /*---Methods---*/
+    @Override
+    public void onEnable() {
+
+    }
+
+    @Override
+    public void onDisable() {
+
+    }
+}
