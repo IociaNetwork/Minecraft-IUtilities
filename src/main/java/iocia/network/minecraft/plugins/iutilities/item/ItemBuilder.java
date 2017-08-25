b05300d282a3131ed7261ebe19e5b4d14ad450c3
Unknown <IociaNetwork@gmail.com>
2017-08-25 11:44:47 -0600

    Added base abstract definitions for builder classes

---
+++ src/main/java/iocia/network/minecraft/plugins/iutilities/item/ItemBuilder.java
@@ -0,0 +1,24 @@
+package iocia.network.minecraft.plugins.iutilities.item;
+
+import org.bukkit.inventory.ItemStack;
+
+public abstract class ItemBuilder<T extends ItemBuilder<T>> {
+    /*---Constructors---*/
+    public ItemBuilder() {
+
+    }
+    
+    /*---Methods---*/
+    /**
+     * Returns the instance of the derivative class.
+     * This is used to allow chaining the builder methods.
+     * @return Instance of derivative class.
+     */
+    protected abstract T me();
+
+    /**
+     * Builds the {@link ItemStack} with the current build properties.
+     * @return New {@link ItemStack}.
+     */
+    public abstract ItemStack build();
+}
