29301334b36f041c8d14e4c6637f662c381bf6a2
Unknown <IociaNetwork@gmail.com>
2017-09-18 15:29:47 -0600

    Finished base interface abstraction for item builders

--- src/main/java/iocia/network/minecraft/plugins/iutilities/item/itembuilder/abstractions/ItemBuilder.java
+++ src/main/java/iocia/network/minecraft/plugins/iutilities/item/itembuilder/abstractions/ItemBuilder.java
@@ -1,10 +1,12 @@
 package iocia.network.minecraft.plugins.iutilities.item.itembuilder.abstractions;
 
 import org.bukkit.enchantments.Enchantment;
+import org.bukkit.inventory.ItemFlag;
 import org.bukkit.inventory.ItemStack;
 
 import java.util.Collection;
 import java.util.Map;
+import java.util.Set;
 
 /**
  * Base abstraction for all {@linkplain org.bukkit.inventory.ItemStack item} builders.
@@ -21,19 +23,16 @@ public interface ItemBuilder<T extends ItemBuilder<T>> {
      * @param enchantment {@link Enchantment} to add.
      * @param level Level of the enchantment.
      * @return ItemBuilder to continue chaining build methods.
-     * @throws IllegalArgumentException Thrown when the given {@linkplain Enchantment enchantment} is null,
-     * or the given level is less than one.
      */
-    T addSafeEnchantment(Enchantment enchantment, int level) throws IllegalArgumentException;
+    T addSafeEnchantment(Enchantment enchantment, int level);
 
     /**
      * Adds a {@linkplain Map map} of <b>safe</b> {@linkplain Enchantment enchantments} to the item.
      * A safe enchantment will not allow the enchantment level cap to be broken.
      * @param enchantments {@linkplain Enchantment Enchantments} to add.
      * @return ItemBuilder to continue chaining build methods.
-     * @throws IllegalArgumentException Thrown when the given enchantment map is null.
      */
-    T addSafeEnchantments(Map<Enchantment, Integer> enchantments) throws IllegalArgumentException;
+    T addSafeEnchantments(Map<Enchantment, Integer> enchantments);
 
     /**
      * Add an <b>unsafe</b> {@linkplain Enchantment enchantment} to the item.
@@ -43,19 +42,16 @@ public interface ItemBuilder<T extends ItemBuilder<T>> {
      * @param enchantment {@link Enchantment} to add.
      * @param level Level of the enchantment.
      * @return ItemBuilder to continue chaining build methods.
-     * @throws IllegalArgumentException Thrown when the given {@linkplain Enchantment enchantment} is null,
-     * or the given level is less than one.
      */
-    T addUnsafeEnchantment(Enchantment enchantment, int level) throws IllegalArgumentException;
+    T addUnsafeEnchantment(Enchantment enchantment, int level);
 
     /**
      * Adds a {@linkplain Map map} of <b>unsafe</b> {@linkplain Enchantment enchantments} to the item.
      * An unsafe enchantment will allow the enchantment level cap to be broken.
      * @param enchantments {@linkplain Enchantment Enchantments} to add.
      * @return ItemBuilder to continue chaining build methods.
-     * @throws IllegalArgumentException Thrown when the given enchantment map is null.
      */
-    T addUnsafeEnchantments(Map<Enchantment, Integer> enchantments) throws IllegalArgumentException;
+    T addUnsafeEnchantments(Map<Enchantment, Integer> enchantments);
     //endregion
     //region Lore
     /**
@@ -63,9 +59,8 @@ public interface ItemBuilder<T extends ItemBuilder<T>> {
      * The lore will appear on the item in the order in which it is added.
      * @param lore Lore to add.
      * @return ItemBuilder to continue chaining build methods.
-     * @throws IllegalArgumentException Thrown when the given lore is null.
      */
-    T addLore(String lore) throws IllegalArgumentException;
+    T addLore(String lore);
 
     /**
      * Adds a {@linkplain Collection collection} of lore to the item.
@@ -73,10 +68,48 @@ public interface ItemBuilder<T extends ItemBuilder<T>> {
      * will be added to the item in order.
      * @param lore Lore to add.
      * @return ItemBuilder to continue chaining build methods.
-     * @throws IllegalArgumentException Thrown when the given lore is null.
      */
-    T addLore(Collection<String> lore) throws IllegalArgumentException;
+    T addLore(Collection<String> lore);
+
+    /**
+     * Adds and array of {@linkplain String strings} to add as lines of lore
+     * on the item. The order in which the strings are set in the array, is the
+     * order the strings will be added to the item as lore.
+     * @param lore Array of strings to add.
+     * @return ItemBuilder to continue chaining build methods.
+     */
+    T addLore(String... lore);
     //endregion
+    //region ItemFlags
+    /**
+     * Adds an {@linkplain ItemFlag item flag} to the item being built.
+     * @param itemFlag Item flag to add.
+     * @return ItemBuilder to continue chaining build methods.
+     */
+    T addItemFlag(ItemFlag itemFlag);
+
+    /**
+     * Adds a {@linkplain Set set} of {@linkplain ItemFlag item flags}
+     * to the item being built.
+     * @param itemFlags Item flags to add.
+     * @return ItemBuilder to continue chaining build methods.
+     */
+    T addItemFlags(Set<ItemFlag> itemFlags);
+    //endregion
+    /**
+     * Sets the display name of the item. The display name is the name
+     * of the item that will be seen by players who hover over the item.
+     * @param displayName Display name to set.
+     * @return ItemBuilder to continue chaining build methods.
+     */
+    T setDisplayName(String displayName);
+
+    /**
+     * Sets the localized name of the item.
+     * @param localizedName Localized name to set.
+     * @return ItemBuilder to continue chaining build methods.
+     */
+    T setLocalizedName(String localizedName);
     //endregion
 
     /**
