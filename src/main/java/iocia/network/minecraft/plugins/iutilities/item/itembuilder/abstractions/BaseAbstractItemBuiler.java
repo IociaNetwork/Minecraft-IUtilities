2ef6154ce1b26831531d8f270ebeb66800a7027c
Unknown <IociaNetwork@gmail.com>
2017-09-18 15:34:19 -0600

    Added BaseAbstractItemBuilder

---
+++ src/main/java/iocia/network/minecraft/plugins/iutilities/item/itembuilder/abstractions/BaseAbstractItemBuiler.java
@@ -0,0 +1,10 @@
+package iocia.network.minecraft.plugins.iutilities.item.itembuilder.abstractions;
+
+public abstract class BaseAbstractItemBuiler<T extends BaseAbstractItemBuiler<T>> implements ItemBuilder<T> {
+    /*---Data---*/
+    protected String displayName = null;
+    
+    /*---Constructors---*/
+    
+    /*---Methods---*/
+}
